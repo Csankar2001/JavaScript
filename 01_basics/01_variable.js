72ebf3a4c52ead8dbeccad0106bf852edfc05e8f
Csankar2001 <chandan2001roy@gmail.com>
2024-05-28 22:11:29 +0530

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 12355; // This will remain unchanged
+let accountEmail = "chndan@gmail.com"; // 'accountemail' should be camelCase for better readability
+let accountPassword = "password"; // Use 'let' instead of 'var' for block scoping and fuction scope
+accountCity = "kolkata"; // Explicitly declare with 'let' for block scoping
+let accountstate;
+//accountId = 2 //not allowed
+accountEmail = "kolkata@gmail.com";
+accountCity = "medinipur";
+console.log("accountId");
+console.table([accountId, accountCity,  accountPassword, accountEmail,accountstate]);
